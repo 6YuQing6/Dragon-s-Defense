a8dc7667a23a8e10339d1c1f2c159401053a6b30
BuildTools <unconfigured@null.spigotmc.org>
2023-06-21 21:46:24 -0700

    fix

--- scripts/Knight.gd
+++ scripts/Knight.gd
@@ -1,16 +1,28 @@
-extends "res://scripts/columns.gd"
+extends KinematicBody2D
 
-var column = 1
-export var speed = 5
-var dodgeSpeed = 25
-var health = 3
-var pos = self.position.x
+var columnsXPosfile
+var columnsXPos
 
+var column = 0 #starting column
+export var speed = 5 #speed it walks down
+var dodgeSpeed = 25 #how fast it dodges
+var health = 3 #number of hits it can take
+var pos = self.position.x #starting pos
+
+#child nodes
+var Animator
+var Sprites
+
+#timer for various things
 var t = Timer.new()
 
 func _ready():
+	columnsXPosfile = get_node("/root/global") # how to get golbal vals copy this
+	columnsXPos = columnsXPosfile.columnsXPos
+	Animator = get_node("AnimationPlayer")
+	Sprites = get_node("Sprite")
 	#print(self.position)
-	self.position = Vector2(columnsXPos[column], -64)
+	self.position = Vector2(columnsXPos[column], -64) # teleports to the top and the starting column
 	#print(self.position)
 
 var no = false
@@ -19,7 +31,13 @@ func _process(delta):
 	self.position.y += speed * delta
 	if Input.is_mouse_button_pressed(1) && !no:
 		#print("pressed")
-		dodge(0, delta)
+		#dodge(1, delta)
+		takeDmg(3)
+	if self.position.y >= 32:
+		print("ATTTTACKKKKKK")
+		atk()
+		#maybe call dragon take dmg func????? MKAE IT SUNYNN!
+	
 
 
 func getColumn():
@@ -27,23 +45,55 @@ func getColumn():
 	return column
 
 func takeDmg(damage):
+	print(health)
+	speed = 0
+	Sprites.set_frame(5)
 	#substracts health by int damage || and checks if it has died
+	t.set_wait_time(0.5)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+
 	health -= damage
+	if health <= 0:
+		print("ded")
+		die()
+		return
+	speed = 5
+	Animator.play("KnightAttack")
+
+func die():
+	Animator.play("die")
+
+func atk():
+	speed = 0
+	Animator.play("atk")
+
+func predict():
+	#MAKE THE ATTACKS SO I CAN DO THIS
+	pass
 
 func dodge(destinationColumn, delta):
 	#smoothy moves the obj to the desired coluinmb using while loops and timers def not bad code :)))
 	no = true
-	$AnimationPlayer.stop()
-	$Sprite.set_frame(2)
+	Animator.stop()
+	#checks the direction shuold the kinght dodge
 	if self.position.x <columnsXPos[destinationColumn]:
+		Sprites.set_frame(3)
+		#solwy moves the spirte to the dest column
 		while self.position.x < columnsXPos[destinationColumn]:
 			self.position.x += dodgeSpeed * delta
+			#BAD CODE PLZ DO NOT COPY it pauses so that the sprite is rendered
 			t.set_wait_time(0.01)
 			t.set_one_shot(true)
 			self.add_child(t)
 			t.start()
 			yield(t, "timeout")
+	#reapeat of the above code 
 	if self.position.x >columnsXPos[destinationColumn]:
+		Sprites.set_frame(3)
+		Sprites.set_flip_h(true)
 		while self.position.x > columnsXPos[destinationColumn]:
 			self.position.x -= dodgeSpeed * delta
 			t.set_wait_time(0.01)
@@ -51,7 +101,21 @@ func dodge(destinationColumn, delta):
 			self.add_child(t)
 			t.start()
 			yield(t, "timeout")
+	#Failsafe to just teleport the kinght to the correct pos
 	self.position = Vector2(columnsXPos[destinationColumn], self.position.y)
-	$Sprite.set_frame(0)
-	$AnimationPlayer.play("KnightAttack")
+
+	#animation for rolling
+	Sprites.set_frame(4)
+
+	t.set_wait_time(0.5)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+
+	Sprites.set_frame(0)
+	Sprites.set_flip_h(false)
+
+	#return to normal walk animation
+	Animator.play("KnightAttack")
 	no = false
